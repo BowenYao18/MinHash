fb6ce7466c6e3a95f99ee7b5abb290fc626fe05f
Bowen Yao <112051015+BowenYao18@users.noreply.github.com>
2023-05-15 17:12:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-record = list(SeqIO.parse("HiSeq_accuracy.fa", "fasta"))
+record = list(SeqIO.parse("/scratch1/zx22/bio/kraken_simulate/MiSeq_accuracy.fa", "fasta"))
 
 ## Populate the list
 lst = []
 id = []
-for i in range(len(record)):
-        lst.append(str(record[i].seq))
-        id.append(str(record[i].id))
+length = 1000
+arr = np.random.permutation(len(record))[:length]
+for i in range(length):
+        lst.append(str(record[arr[i]].seq))
+        id.append(str(record[arr[i]].id))
 # print(lst)
 # print(id)
 
@@ -53,11 +55,10 @@ def prob_dist(matrix, list_seq, hash_range=20, time=20, k=5):
         s = sum(output)
         return [x / s for x in output]
 
-global_matrix = build_matrix(lst, hash_range=20, time=20, k=5)
+global_matrix = build_matrix(lst, hash_range=200, time=20, k=10)
 #print(global_matrix)
-probability = prob_dist(global_matrix, lst, hash_range=20, time=20, k=5)
+probability = prob_dist(global_matrix, lst, hash_range=200, time=20, k=10)
 #print(probability)
-
 
 def random_unique(id, pick_num):
         random_elements = random.sample(id, pick_num)
@@ -81,66 +82,68 @@ def check_unique_species(id, pick_num, probability):
         #print(unique)
         return len(unique)
 
-# print("Random selction result: ", random_unique(id, 10))
-# print("Algorithm selction result: ", check_unique_species(id, 10, probability))
-
-# def test(time, pick_num):
-#         count = 0
-#         for i in range(time):
-#                 if (random_unique(id, pick_num) < check_unique_species(id, pick_num, probability)):
-#                         count += 1
-#         return count/time
-
-# print(test(100, 5))
+print("Random selction result: ", random_unique(id, 10))
+print("Algorithm selction result: ", check_unique_species(id, 10, probability))
 
 
-def compare(str1, str2, k, hash_range, time):
-        same = 0
-        for seed in range(time):
-                min1 = hash_min(str1, k, seed, hash_range)
-                min2 = hash_min(str2, k, seed, hash_range)
-                # print("min1",min1,"min2",min2)
-                if (min1 == min2):
-                        same += 1
-        return same / time
 
-def jaccard_similarity(str1, str2, k):
-        set1 = set()
-        set2 = set()
-        for i in range(len(str1) - k + 1):
-                set1.add(str1[i : i + k])
-        for j in range(len(str2) - k + 1):
-                set2.add(str2[j : j + k])
-        intersection = set1.intersection(set2)
-        union = set1.union(set2)
-        # print(union)
-        # print(intersection)
-        return len(intersection) / len(union)
+def test(time, pick_num):
+        count = 0
+        for i in range(time):
+                if (random_unique(id, pick_num) < check_unique_species(id, pick_num, probability)):
+                        count += 1
+        return count/time
+
+print(test(100, 10))
+
+
+# def compare(str1, str2, k, hash_range, time):
+#         same = 0
+#         for seed in range(time):
+#                 min1 = hash_min(str1, k, seed, hash_range)
+#                 min2 = hash_min(str2, k, seed, hash_range)
+#                 # print("min1",min1,"min2",min2)
+#                 if (min1 == min2):
+#                         same += 1
+#         return same / time
+
+# def jaccard_similarity(str1, str2, k):
+#         set1 = set()
+#         set2 = set()
+#         for i in range(len(str1) - k + 1):
+#                 set1.add(str1[i : i + k])
+#         for j in range(len(str2) - k + 1):
+#                 set2.add(str2[j : j + k])
+#         intersection = set1.intersection(set2)
+#         union = set1.union(set2)
+#         # print(union)
+#         # print(intersection)
+#         return len(intersection) / len(union)
 
 # print(jaccard_similarity(dna1, dna2, 3))
 # print(compare(dna1, dna2, 3, 1000000000, 10000))
 
-def jaccard_matrix(lst, k):
-        '''Return the jaccard distance matrix for all sequence. '''
-        length = len(lst)
-        jaccard_mat = np.zeros((length, length))
-        for i in range(length):
-                for j in range(i, length):
-                        sim = jaccard_similarity(lst[i], lst[j], k)
-                        jaccard_mat[i, j] = sim
-                        jaccard_mat[j, i] = sim
-        return jaccard_mat
+# def jaccard_matrix(lst, k):
+#         '''Return the jaccard distance matrix for all sequence. '''
+#         length = len(lst)
+#         jaccard_mat = np.zeros((length, length))
+#         for i in range(length):
+#                 for j in range(i, length):
+#                         sim = jaccard_similarity(lst[i], lst[j], k)
+#                         jaccard_mat[i, j] = sim
+#                         jaccard_mat[j, i] = sim
+#         return jaccard_mat
 
-jaccard_mat = jaccard_matrix(lst, k=5)
+# jaccard_mat = jaccard_matrix(lst, k=5)
 
-fig, ax = plt.subplots()
-heatmap = ax.pcolor(jaccard_mat, cmap=plt.cm.Blues)
-plt.colorbar(heatmap)
+# fig, ax = plt.subplots()
+# heatmap = ax.pcolor(jaccard_mat[:length, :length], cmap="bwr")
+# plt.colorbar(heatmap)
 
-# Show values in each cell
-for i in range(len(id)):
-    for j in range(len(id)):
-        value = "{:.2f}".format(jaccard_mat[i, j])
+# # Show values in each cell
+# for i in range(len(id)):
+#     for j in range(len(id)):
+#         value = "{:.2f}".format(jaccard_mat[i, j])
 
-plt.tight_layout()
-plt.savefig("heatmap.png")
+# plt.tight_layout()
+# plt.savefig("heatmap1.png")
